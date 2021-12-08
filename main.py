5a90fa995d99c21a7dcadf597907ebf2ca379492
solos-pro <sergey-solonichenko@rambler.ru>
2021-12-08 22:31:24 +0300

    search.html Поиск РАБОТАЕТ

--- main.py
+++ main.py
@@ -25,6 +25,19 @@ def candidates_():
         candidates = json.load(f)
     return render_template("list.html", users=candidates)
 
+@app.route("/search/")
+def search_page():
+    name = request.args.get("name")
+    with open("candidates.json") as f:
+        candidates = json.load(f)
+    users = []
+    if name:
+        for candidate in candidates:
+            if name in candidate["name"]:
+                users.append(candidate["name"])
+
+    return render_template("search.html", users=users, cnt=len(users))
+
 
 if __name__ == "__main__":
     app.run()
